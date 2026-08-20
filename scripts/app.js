50345553e166fd034da23de9c44b3bc360a1be9d
Lara Yasmin da Silva Cunha <yasmin.lara@escolar.ifrn.edu.br>
2026-08-20 13:17:26 -0300

    java

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
-alert("olá mundo")
+alert("olá mundo!")
+
+var nasc = 2010;          
+let nome = "Lara";    
+const viva = true;        
+let altura = 1.61;        
+
+if (viva) {
+    let saudacao = "Olá, " + nome + "!";
+
+    let mensagem = `Altura: ${altura}m | Idade: ${2025-nasc}`;
+    alert(saudacao+'\n'+mensagem);
+} else {
+
+    alert("Você não está viva!");
+}
