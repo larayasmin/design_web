cb4b4ca8eb68e659bd82fa8303d0769e181d3d40
Lara Yasmin da Silva Cunha <yasmin.lara@escolar.ifrn.edu.br>
2026-08-26 17:56:10 -0300

    alterei

---
+++ scripts/app3.js
@@ -0,0 +1,15 @@
+let nasc = 2010;       
+let nome = "Lara";   
+let viva = true;        
+let altura = 1.61;      
+let sexo = undefined;   
+let peso = null;       
+let imc = "peso"*10;   
+
+alert(`Tipo de nasc: ${typeof nasc}`);
+alert(`Tipo de nome: ${typeof nome}`);
+alert(`Tipo de viva: ${typeof viva}`);
+alert(`Tipo de altura: ${typeof altura}`);
+alert(`Tipo de sexo: ${typeof sexo}`);
+alert(`Tipo de peso: ${typeof peso}`);
+alert(`Tipo de imc: ${typeof imc}`);
