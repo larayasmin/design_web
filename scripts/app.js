79a8f9cae91d5b1961ff3db035894211f206cad5
Lara Yasmin da Silva Cunha <yasmin.lara@escolar.ifrn.edu.br>
2026-08-26 17:40:38 -0300

    fiz coisas

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,21 @@ const viva = true;
 
 function calcIdade(ano=2026){
     let idade = ano - nasc;
-    alert(`Dentro de Função - Idade ${idade}`);
-    return idade;
+    let menor;
+    if (idade < 18){
+        menor = true;
+        var podebeber = false
+    }else{
+        menor = false;
+        var podebeber = true;
+    }
+        alert(`${nome} é menor de idade ${menor}/nIdade: ${idade}/nPode beber: ${podebeber}`)
 }
 
 calcIdade();
+/*
+var vaza a variavel dentro do escopo da mesma funcao
+alert(`Pode beber: ${podebeber}`)
 /*
 alert('Fora da fução : Idade ${idade}');
 Erro pois a variavel let não existe fora do escopo dela
