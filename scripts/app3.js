fbccb1bd83cc34bb8540fa4c44a179bb444fc377
Lara Yasmin da Silva Cunha <yasmin.lara@escolar.ifrn.edu.br>
2026-08-28 13:12:26 -0300

    a

--- scripts/app3.js
+++ scripts/app3.js
@@ -6,10 +6,10 @@ let sexo = undefined;
 let peso = null;       
 let imc = "peso"*10;   
 
-alert(`Tipo de nasc: ${typeof nasc}`);
-alert(`Tipo de nome: ${typeof nome}`);
-alert(`Tipo de viva: ${typeof viva}`);
-alert(`Tipo de altura: ${typeof altura}`);
-alert(`Tipo de sexo: ${typeof sexo}`);
-alert(`Tipo de peso: ${typeof peso}`);
-alert(`Tipo de imc: ${typeof imc}`);
+alert(`Tipo de nasc: ${typeof (nasc)}`);
+alert(`Tipo de nome: ${typeof (nome)}`);
+alert(`Tipo de viva: ${typeof (viva)}`);
+alert(`Tipo de altura: ${typeof (altura)}`);
+alert(`Tipo de sexo: ${typeof (sexo)}`);
+alert(`Tipo de peso: ${typeof (peso)}`);
+alert(`Tipo de imc: ${typeof (imc)}`);
