12c069ceb1c294045a4aadc8b025e6b7674f9610
Lara Yasmin da Silva Cunha <yasmin.lara@escolar.ifrn.edu.br>
2026-09-02 18:00:05 -0300

    a

---
+++ js/app3.js
@@ -0,0 +1,25 @@
+let dia = prompt("Escolha um dia da semana\nSendo 1:Domingo - 7:Sábado");
+dia = Number(dia);
+switch(dia){
+    case 1: alert("Você escolheu domingo")
+    case 2: alert("Você escolheu segunda")
+    case 3:
+}
+
+if (dia <= 0 || dia >= 8){
+    alert("Dia inválido")
+}else if(dia == 1){
+
+}else if(dia == 2){
+    
+}else if(dia == 3){
+    alert("Você escolheu terça")
+}else if(dia == 4){
+    alert("Você escolheu quarta")
+}else if(dia == 5){
+    alert("Você escolheu quinto")
+}else if(dia == 6){
+    alert("Você escolheu sexta")
+}else if(dia == 7){
+    alert("Você escolheu sabado")
+}
