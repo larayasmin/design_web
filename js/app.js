aa5f9120a5c8d5be0353da926ff747134e6d3bc2
Lara Yasmin da Silva Cunha <yasmin.lara@escolar.ifrn.edu.br>
2026-09-04 13:51:58 -0300

    atualizei

--- js/app.js
+++
@@ -1,9 +0,0 @@
-let num  = Number(prompt("Digite um número: "));
-
-if (num % 2 != 0){
-    alert(`${num} é impar.`);
-}else if(num == 0){
-    alert(`${num} é zero.`);
-}else{
-    alert(`${num} é par.`);
-}
--- js/app1.js
+++ js/app1.js
@@ -2,7 +2,7 @@ let vezes = prompt("Digite a quantidade de vezes: ");
 vezes = Number(vezes)
 let i = 1;
 
-do{
+while(i <= vezes){
     alert(`Contei ${i} vezes`);
     i = i + 1;
-}while(i <= vezes);
+}
