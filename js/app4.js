d69ba6c5f8453ca495caedc391a6f39e7ab734e6
Lara Yasmin da Silva Cunha <yasmin.lara@escolar.ifrn.edu.br>
2026-09-04 14:26:31 -0300

    atualizei

--- js/app4.js
+++ js/app4.js
@@ -1,4 +1,12 @@
 let vezes = Number(prompt("Digite o número de vezes: "));
 for (let i = 0; i <= vezes; i++){
-    alert(`Contei ${i} vezes.`);
+    if (vezes > 100){
+        alert("Valor inválido, digite um valor de 0 a 100: ");
+        break
+    }
+    alert(`Contei ${i} vezes.`)
+    if (i % 2 != 0){ //é impar?
+        continue; //se impar, continua = voltar para o início.
+    }
+    alert(`${i} é par.`);
 }
