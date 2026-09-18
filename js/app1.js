50ac4d4c4bfa955095f930d1ff38dc4e885dcf8b
Lara Yasmin da Silva Cunha <yasmin.lara@escolar.ifrn.edu.br>
2026-09-18 13:22:16 -0300

    alterei

---
+++ js/app1.js
@@ -0,0 +1,38 @@
+let i = 1;
+const imagem = document.getElementById("foto");
+
+function proximo(e){
+    e.preventDefault();
+    if (i < 6){
+        i = i + 1;
+    }
+    imagem.setAttribute("src",`./img/foto${i}.jpg`);
+}
+
+const prox = document.getElementById("prox");
+prox.addEventListener("click", proximo);
+
+function anterior(e){
+    e.preventDefault();
+    if(i>1){
+        i = i - 1;
+    }
+    imagem.setAttribute("src",`./img/foto${i}.jpg`);
+}
+
+const ant = document.getElementById("ant");
+ant.addEventListener("click", anterior);
+
+function selecionar(e){
+    e.preventDefault();
+    i = cbbox.value;
+    imagem.setAttribute("src",`./img/foto${i}.jpg`);
+}
+const cbbox = document.getElementById("combobox");
+cbbox.addEventListener("change", selecionar);
+
+function mudar(e, foto){
+    e.preventDefault();
+    i = foto;
+    imagem.setAttribute("src",`./img/foto${i}.jpg`);
+}
