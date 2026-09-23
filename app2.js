78d4ad41a5f4a75e5171373e3abc11565b01cfe8
Lara Yasmin da Silva Cunha <yasmin.lara@escolar.ifrn.edu.br>
2026-09-23 17:56:49 -0300

    alterei

--- app2.js
+++ app2.js
@@ -1,1 +1,40 @@
 const amigos = []
+const cadastro = document.getElementById("Cadastro");
+const nome = cadastro.nome;
+const nasc = cadastro.nasc;
+const whatsapp = cadastro.whatsapp;
+const lista = document.getElementById("lista")
+
+cadastro.addEventListener("submit", function(l){
+    l.preventDefault()
+    let item = [nome.value, nasc.value, whatsapp.value];
+    amigos.unshift(item);
+
+    cadastro.reset(); //LIMPA O FORMULARIO
+    exibirLista(); //ATUALIZA A LISTA
+});
+
+function exibirLista(){
+    let itens = "";
+    for (let i = 0; i < amigos.length; i ++){
+        let item = amigos[i] //Item = [Nome, Nasc, Whatsapp]
+        
+        let remover = `<button onclick = remover (${i})> Remover</button>`
+
+        //CRIA UMA TAG LI
+        let li = `<li> ${item[0]} | ${item[1]} | ${item[2]}  ${remover} </li>`;
+        //JUNTA O LI NOS ITENS
+        itens = itens + li;
+    }
+    //ALTERAR O HTML DA LISTA PARA SER IGUAL AOS ITENS
+    lista.innerHTML = itens;
+}
+
+function remover(i){
+    let item = amigos[i]; //[Nome, Nasc, Whatapp]]
+    let chekc = confirm(`Deseja realmente excluir ${item[0]}?`);
+    if (check == true){
+        amigos.splice{i,1}; //splice(posicao inicial, qtd itens a remover)
+    }
+    exibirLista();
+}
